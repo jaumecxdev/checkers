decede59a4b88a610c27ce5da3b2e7e640787048
jaumecxdev <jaumecx@gmail.com>
2023-04-22 09:38:44 +0200

    230422-6-play-game

--- x/checkers/types/codec.go
+++ x/checkers/types/codec.go
@@ -8,17 +8,17 @@ import (
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgCreatePost{}, "checkers/CreatePost", nil)
 	cdc.RegisterConcrete(&MsgCreateGame{}, "checkers/CreateGame", nil)
+	cdc.RegisterConcrete(&MsgPlayMove{}, "checkers/PlayMove", nil)
 	// this line is used by starport scaffolding # 2
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
-		&MsgCreatePost{},
+		&MsgCreateGame{},
 	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
-		&MsgCreateGame{},
+		&MsgPlayMove{},
 	)
 	// this line is used by starport scaffolding # 3
 
