2878eb6808650a0d04eac4e425c2e5120e0525b7
jaumecxdev <jaumecx@gmail.com>
2023-04-22 09:55:14 +0200

    230422-7-events

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -7,8 +7,11 @@ const (
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
 
-	// RouterKey defines the module's message routing key
+	// RouterKey is the message route for slashing
 	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
 
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_checkers"
@@ -19,5 +22,22 @@ func KeyPrefix(p string) []byte {
 }
 
 const (
-	SystemInfoKey = "SystemInfo/value/"
+	SystemInfoKey = "SystemInfo-value-"
+)
+
+const (
+	GameCreatedEventType      = "new-game-created" // Indicates what event type to listen to
+	GameCreatedEventCreator   = "creator"          // Subsidiary information
+	GameCreatedEventGameIndex = "game-index"       // What game is relevant
+	GameCreatedEventBlack     = "black"            // Is it relevant to me?
+	GameCreatedEventRed       = "red"              // Is it relevant to me?
+)
+
+const (
+	MovePlayedEventType      = "move-played"
+	MovePlayedEventCreator   = "creator"
+	MovePlayedEventGameIndex = "game-index"
+	MovePlayedEventCapturedX = "captured-x"
+	MovePlayedEventCapturedY = "captured-y"
+	MovePlayedEventWinner    = "winner"
 )
